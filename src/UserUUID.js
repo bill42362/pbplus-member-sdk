b70cff345cd3e5753b2d2d691830137bc576b049
Bill42362 <bill42362@gmail.com>
2017-09-07 18:51:30 +0800

    Enable sync user uuid with searches/cookies.

--- src/UserUUID.js
+++ src/UserUUID.js
@@ -1,15 +1,43 @@
 // UserUUID.js
 'use strict';
+import { getUrlSearches, makeSearchString, newUuid, saveCookieByName, getCookieByName } from './Utils.js';
 
-const defaultState = 'mock-user-uuid';
+const COOKIE_NAME = 'tokenId';
+
+const searches = getUrlSearches()
+const uuidFromSearch = searches[COOKIE_NAME];
+delete searches[COOKIE_NAME];
+const newSearchString = makeSearchString(searches);
+//window.history.replaceState('', '', `${location.pathname}${newSearchString ? '?' : ''}${newSearchString}`);
+
+const uuidFromCookie = getCookieByName({name: COOKIE_NAME});
+const defaultState = uuidFromSearch || uuidFromCookie || newUuid();
+
+const saveUuidToCookie = ({ uuid }) => {
+    const dateNow = new Date();
+    const dateTwoWeeksLater = new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + 14);
+    saveCookieByName({name: COOKIE_NAME, data: uuid, expireDate: dateTwoWeeksLater, domain: '.pbplus.me'});
+};
+
+if(!uuidFromCookie || (uuidFromSearch && uuidFromSearch !== uuidFromCookie)) {
+    saveUuidToCookie({uuid: defaultState});
+}
 
 const Reducer = (state = defaultState, action) => {
     switch(action.type) {
+        case 'UPDATE_PBPLUS_USER_UUID':
+            return action.payload.uuid || state;
+            break;
         default:
             return state;
     }
 }
 
-const Actions = { };
+const updateUserUUID = ({ uuid }) => {
+    saveUuidToCookie({ uuid });
+    return {type: 'UPDATE_PBPLUS_USER_UUID', payload: { uuid }};
+};
+
+const Actions = { updateUserUUID };
 
 export default { Reducer, Actions };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // index.js
 import MemberCenter from './MemberCenter.js';
+import UserUUID from './UserUUID.js';
 import MemberCenterContainer from './MemberCenterContainer.js';
 
 const Actions = {
     display: MemberCenter.Actions.display,
+    updateUserUUID: UserUUID.Actions.updateUserUUID,
 };
 
 export default {
